fad09e930b9e6be61c9b810d87d252c4d7c9f567
maxproft <proft.max@gmail.com>
2017-02-02 13:48:47 +1100

    updated install doc, made file to auto install things

---
+++ installubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get --yes remove unity-webapps-common #amazon
+sudo apt-get --yes remove indicator-messages #envelope
+sudo apt-get --yes install vlc liferea wine cowsay audacity chromium-browser stellarium geogebra youtube-dl gummi ubuntu-restricted-extras gtk-recordmydesktop gimp kolourpaint4 openshot pdfshuffler vim git anki synaptic vnstat openssh-client openssh-server
+
+
+sudo add-apt-repository --yes ppa:ian-berke/ppa-drawers
+sudo apt-get --yes update
+sudo apt-get --yes install drawers
+
+
+#Python packages
+sudo apt-get --yes install python-imaging python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-matplotlib python-opencv
+
+sudo apt-get --yes update
+sudo apt-get --yes upgrade
