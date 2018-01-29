be9d38800a13e5f15b3e5585980cb53ac6e72c3e
maxproft <proft.max@gmail.com>
2018-01-29 07:11:43 +0000

    Add github account

--- installubuntu.sh
+++ installubuntu.sh
@@ -9,6 +9,10 @@ sudo add-apt-repository --yes ppa:ian-berke/ppa-drawers
 sudo apt-get --yes update
 sudo apt-get --yes install drawers
 
+#add in github account
+git config --global user.email “email@address”
+git config --global user.name “maxproft”
+
 
 #Python packages
 sudo apt-get --yes install python-imaging python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-matplotlib python-opencv
